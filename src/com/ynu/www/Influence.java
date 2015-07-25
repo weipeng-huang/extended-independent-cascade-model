5805ae47ef19efab66648fe9365a4f204dd10a7c
believejava <chinawphuang@163.com>
2015-07-25 23:42:27 +0800

    just test github commit

--- src/com/ynu/www/Influence.java
+++ src/com/ynu/www/Influence.java
@@ -14,6 +14,7 @@ public class Influence {
    */
   public static void main(String[] args) {
     // TODO Auto-generated method stub
+    // just test github commit
     System.out.println("Hello world!!!");
 
   }
