10efa36d45163c06f48247d90a6dc969541ba263
believejava <chinawphuang@163.com>
2015-07-25 20:42:35 +0800

    begin to start coding

---
+++ src/com/ynu/www/Influence.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.ynu.www;
+
+/**
+ * @author Wphuang
+ *
+ */
+public class Influence {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hello world!!!");
+
+  }
+
+}
